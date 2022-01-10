923a54ebc4fa5af02be0d92b24f5744194284ced
Ritwik880 <rittiksinha12@gmail.com>
2022-01-10 13:42:12 +0530

    debugging status 442

---
+++ eebow/src/components/login.component.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import login from '../image/login_secure.svg'
+
+export default class Login extends Component {
+    render() {
+        return (
+            <section className="login">
+                <div class="row justify-content-center">
+                <div class="col-6 column">
+                <form>
+                    <h3 className="si_h3">Sign In</h3>
+
+                    <div className="form-group">
+                        <label>Email address <span>*</span></label>
+                        <input type="email" className="form-control" placeholder="Enter email" />
+                    </div>
+
+                    <div className="form-group">
+                        <label>Password <span>*</span></label>
+                        <input type="password" className="form-control" placeholder="Enter password" />
+                    </div>
+
+                    <div className="form-group">
+                        <div className="custom-control custom-checkbox">
+                            <input type="checkbox" className="custom-control-input" id="customCheck1" />
+                            <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
+                        </div>
+                    </div>
+
+                    <button type="submit" className="btn btn-primary btn-block">Submit</button>
+                    <p className="forgot-password text-right">
+                        Forgot <a href="#">password?</a>
+                    </p>
+                    <p className="forgot-password text-right">
+                        Not registered <a href="/signup">Register?</a>
+                    </p>
+                </form>
+                </div>
+                <div class="col-6 column">
+                <img src={login} />
+                </div>
+                </div>
+            </section>
+        );
+    }
+}
